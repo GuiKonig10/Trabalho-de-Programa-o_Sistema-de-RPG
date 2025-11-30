8bd2d2508ce7a35fcef8c65efeff7fec275ac1ac
Guilherme Otávio Riffel König <konig.gui@gmail.com>
2025-11-30 17:57:35 -0300

    Create Personagem.py

---
+++ Personagem.py
@@ -0,0 +1,46 @@
+class Personagem:
+    def _init_(self, nome, nivel):
+        self.nome = nome
+        self.nivel = nivel
+        self.hp_max = 100 + nivel * 10
+        self.hp = self.hp_max
+        self.capacidade = 50
+        self.inventario = []
+        self.classe = "Personagem"
+
+    def peso_total(self):
+        return sum(item.peso for item in self.inventario)
+
+    def adicionar_item(self, item):
+        if self.peso_total() + item.peso <= self.capacidade:
+            self.inventario.append(item)
+            print(f"{item.nome} adicionado ao inventário de {self.nome}.")
+        else:
+            print("Inventário cheio!")
+
+    def remover_item(self, nome_item):
+        for item in self.inventario:
+            if item.nome.lower() == nome_item.lower():
+                self.inventario.remove(item)
+                print(f"{item.nome} removido do inventário de {self.nome}.")
+                return
+        print("Item não encontrado.")
+
+    def exibir_inventario(self):
+        print(f"\nInventário de {self.nome} {self.classe} (Peso {self.peso_total()}/{self.capacidade}):")
+
+        if not self.inventario:
+            print("Inventário vazio.")
+        else:
+            for item in self.inventario:
+                print(f"- {item}")
+              def usar_pocao(self):
+        for item in self.inventario:
+            if item.tipo == "poção":
+                self.hp += item.efeito
+                if self.hp > self.hp_max:
+                    self.hp = self.hp_max
+                self.inventario.remove(item)
+                print(f"{self.nome} usou {item.nome} e recuperou {item.efeito} HP! (HP atual: {self.hp})")
+                return
+        print(f"{self.nome} não tem poções!")
